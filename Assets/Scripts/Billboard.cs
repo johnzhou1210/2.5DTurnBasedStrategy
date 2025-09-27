4827492737f93452344bcc491aa65d7b446ad2a1
John Zhou <chowjohn10@gmail.com>
2025-09-27 14:57:03 -0400

    Experimentation with 2.5d perpsective

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    [SerializeField] private Transform targetTransform;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void LateUpdate() {
+        transform.LookAt(targetTransform);
         transform.forward = Camera.main.transform.forward;
+        
     }
 
 }
