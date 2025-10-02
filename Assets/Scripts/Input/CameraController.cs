7ad9d94d36d5ab7371121a75b977ff0e2fefe9ed
John Zhou <chowjohn10@gmail.com>
2025-10-01 20:33:54 -0400

    Camera controls are better but still not great

--- Assets/Scripts/Input/CameraController.cs
+++ Assets/Scripts/Input/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private float cameraMoveSpeed = 5f;
+    [SerializeField] private Vector2 cameraYMinMax = new Vector2(0f, 20f);
+
     
     private CinemachineCamera _vCam;
 
@@ -27,10 +29,12 @@ public class CameraController : MonoBehaviour
         Vector2 moveInput = cameraMoveAction.ReadValue<Vector2>();
         Vector2 lookInput = cameraLookAction.ReadValue<Vector2>();
         _vCam.transform.Translate(new Vector3(moveInput.x, 0f, moveInput.y) * (Time.deltaTime * cameraMoveSpeed));
+        _vCam.transform.position = new Vector3(_vCam.transform.position.x, Mathf.Clamp(_vCam.transform.position.y, cameraYMinMax.x, cameraYMinMax.y), _vCam.transform.position.z); ;
 
         Vector3 raycastPosition = InputDelegates.GetMouseRaycastPosition?.Invoke() ?? transform.position;
         if (raycastPosition == transform.position) return;
-        _vCam.transform.LookAt(raycastPosition * Time.deltaTime * 200f);
+      
+        // _vCam.transform.Rotate(0f, 180f, 0f);
         
     }
 
